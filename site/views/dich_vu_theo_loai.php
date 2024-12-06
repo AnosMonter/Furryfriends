013147e2bb24a8c19f874f5df91f4aea7961cef1
kimyeni <kyen9277@gmail.com>
2024-12-06 23:25:13 +0700

    Update dich_vu_theo_loai.php

--- site/views/dich_vu_theo_loai.php
+++ site/views/dich_vu_theo_loai.php
@@ -1,9 +1,16 @@
 <style>
-    .page-service-site {
+    .serve_general{
         display: flex;
+        /* justify-content: space-around; */
+        margin: 20px 100px;
+        padding: 20px;
+    }
+    .page-service-site {
+        /* display: flex; */
         flex-direction: column;
         align-items: center;
-        padding: 20px;
+        padding: 0 20px;
+        width: 40%;
     }
 
     .page-service-site form {
@@ -11,7 +18,7 @@
         flex-direction: column;
         align-items: center;
         width: 100%;
-        max-width: 400px;
+        max-width: 300px;
     }
 
     .page-service-site select {
@@ -30,78 +37,75 @@
         cursor: pointer;
         margin-top: 10px;
     }
-
-    .service-detail-site {
-        display: grid;
-        grid-template-columns: 1fr 2fr;
-        grid-gap: 20px;
-        background-color: #fff;
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-        padding: 20px;
-        border-radius: 10px;
-    }
-
     .service-detail-site h2 {
-        font-size: 24px;
-        color: #333;
-        margin-bottom: 10px;
-        transition: color 0.3s ease-in-out;
+        margin: 20px 0;
     }
-
-    .service-detail-site h2:hover {
-        color: #007bff;
-    }
-
     .service-detail-price {
         font-size: 20px;
         font-weight: bold;
-    }
-    .service-detail-price p {
+        margin: 10px 10px;
         color: red;
     }
-
     .service-detail-content {
-        font-size: 18px;
+        font-size: 16px;
         line-height: 1.5;
-        font-weight: bold;
+        font-weight:none;
+        margin: 10px 20px;
     }
-
     .service-detail-site img {
-        width: 100%;
+        width: 70%;
         height: auto;
         border-radius: 10px;
+        margin: 20px 50px;
+    } 
+    .server-intro{
+        margin: 20px 20px;
     }
+    .server-intro h1{
+        text-align: center;
+        margin: 10px 0;
+    }
+    
+    
 </style>
 <main>
-    <div class="page-service-site">
-        <form action="index.php?Page=dich_vu&ID=<?= $_GET['ID'] ?>" method="post">
-            Chọn Loại:
-            <select name="Service" id="">
-                <?php
-                $List_DV ='';
-                foreach ($Service as $ser) {
-                    if (isset($Detail_Service) && !empty($Detail_Service) && $Detail_Service['ID'] == $ser['ID']) {
-                        $List_DV .= $ser['Status'] != 0 ? '<option value="' . $ser['ID'] . '" selected>' . $ser['Name'] . '</option>' : '';
-                    } else {
-                        $List_DV .= $ser['Status'] != 0 ? '<option value="' . $ser['ID'] . '">' . $ser['Name'] . '</option>' : '';
+    <div class="serve_general">
+        <div class="page-service-site">
+            <form action="index.php?Page=dich_vu&ID=<?= $_GET['ID'] ?>" method="post">
+                Chọn Loại:
+                <select name="Service" id="">
+                    <?php
+                    $List_DV ='';
+                    foreach ($Service as $ser) {
+                        if (isset($Detail_Service) && !empty($Detail_Service) && $Detail_Service['ID'] == $ser['ID']) {
+                            $List_DV .= $ser['Status'] != 0 ? '<option value="' . $ser['ID'] . '" selected>' . $ser['Name'] . '</option>' : '';
+                        } else {
+                            $List_DV .= $ser['Status'] != 0 ? '<option value="' . $ser['ID'] . '">' . $ser['Name'] . '</option>' : '';
+                        }
                     }
-                }
-                echo !empty($List_DV)? $List_DV:'<option value="">Không Có Dịch Vụ Nào</option>';
-                ?>
-            </select>
-            <button>Xem Chi Tiết Dịch Vụ</button>
-        </form>
+                    echo !empty($List_DV)? $List_DV:'<option value="">Không Có Dịch Vụ Nào</option>';
+                    ?>
+                </select>
+                <button>Xem Chi Tiết Dịch Vụ</button>
+            </form>
+        </div>
+        <div class="server-intro">
+            <h1>Dịch vụ Furry Friends</h1>
+            <span>Tại Furry Friends, chúng tôi hiểu rằng thú cưng không chỉ là những người bạn đồng hành, mà còn là một phần quan trọng trong gia đình bạn. 
+            Vì vậy, chúng tôi tự hào mang đến các dịch vụ chăm sóc thú cưng toàn diện và chuyên nghiệp, giúp các bé luôn khỏe mạnh, hạnh phúc và được yêu thương.</span>
+            
+            <?php
+            if (isset($Detail_Service) && !empty($Detail_Service)) {
+            ?>
+                <div class="service-detail-site">
+                    <h2><?= $Detail_Service['Name'] ?> </h2>
+                    <div class="service-detail-price">Giá Dịch Vụ: <br><p><?= number_format($Detail_Service['Price'], 0, '.', '.') . 'VNĐ/' . $Detail_Service['Time_Service'] ?></p></div>
+                    <div class="service-detail-content"><?= nl2br($Detail_Service['Detail']) ?></div>
+                    <img src="<?= $Detail_Service['Image'] ?>" alt="<?= $Detail_Service['Name'] ?>">
+                </div>
+                </div>
+            <?php
+            } ?>
+        </div>
     </div>
-    <?php
-    if (isset($Detail_Service) && !empty($Detail_Service)) {
-    ?>
-        <div class="service-detail-site">
-            <h2><?= $Detail_Service['Name'] ?> </h2>
-            <div class="service-detail-price">Giá Dịch Vụ: <br><p><?= number_format($Detail_Service['Price'], 0, '.', '.') . 'VNĐ/' . $Detail_Service['Time_Service'] ?></p></div>
-            <div class="service-detail-content"><?= nl2br($Detail_Service['Detail']) ?></div>
-            <img src="<?= $Detail_Service['Image'] ?>" alt="<?= $Detail_Service['Name'] ?>">
-        </div>
-        </div>
-    <?php
-    } ?>
 </main>
